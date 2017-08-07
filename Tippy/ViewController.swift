262275d7f0e6d801e872305a78f7ef57c8d53e39
Diljeet Singh <jeetds1994@gmail.com>
2017-08-06 20:49:14 -0400

    added functionality

--- Tippy/ViewController.swift
+++ Tippy/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var tipBackground: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.right))
+        swipeRight.direction = UISwipeGestureRecognizerDirection.right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.left))
+        swipeRight.direction = UISwipeGestureRecognizerDirection.left
+        self.view.addGestureRecognizer(swipeLeft)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +38,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        let billPercentages = [0.18, 0.2, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        
+        let tip = bill * billPercentages[0]
+        
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        
+        totalLabel.text = String(format: "$%.2f", total)
 
+    }
+    
+    func right(){
+        if tipBackground.text == "18%"{
+            tipBackground.text = "22%"
+        }else if tipBackground.text == "20%"{
+            tipBackground.text = "25%"
+        
+        } else if tipBackground.text == "25%"{
+            
+        
+        }
+    
+    }
+    func left(){
+        if tipBackground.text == "18%"{
+            
+            
+        }else if tipBackground.text == "20%"{
+            tipBackground.text = "18%"
+            
+        } else if tipBackground.text == "25%"{
+            tipBackground.text = "22%"
+        }
+    }
 }
 
